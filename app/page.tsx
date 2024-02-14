0f4ddf4a394d61ba814a53f9c290693419c9e55d
Yorafa <thomasyorafa@gmail.com>
2024-02-13 20:46:35 -0500

    fix path

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@ import Header from "./components/header";
 export default function Home() {
     return <>
         <Header />
-        <img src="/people/tombstone.png"/>
+        <img src="/Cemetery/tombstone.png"/>
     </>;
 }
