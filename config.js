3b81a2911369d7bd80078ffdb06c7448dc519179
Yorafa <thomasyorafa@gmail.com>
2024-02-14 20:43:11 -0500

    setup cors

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const PORT = 8000;
 export const MONGO_URI = "mongodb://localhost:27017/mongodb";
 
 export const CorsOptions = {
-    origin: "http://localhost:3000",
+    origin: "https://home.yorafa.com",
     method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     // credentials: true,
     allowedHeaders: ["Content-Type"],
