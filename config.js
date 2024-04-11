a3938f32b3b209b062c24ad08128c7a3312e1362
Yorafa <thomasyorafa@gmail.com>
2024-04-11 17:38:33 -0400

    try fix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const PORT = 3000;
 export const MONGO_URI = "mongodb+srv://yorafa:sgFBTsgTwusLPCWp@cluster0.c2kr52a.mongodb.net/?retryWrites=true&w=majority";
 
 export const CorsOptions = {
-    origin: "https://cemetery.yorafa.com",
+    origin: "https://cemetery.yorafa.com/",
     method: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     // credentials: true,
     allowedHeaders: ["Content-Type"],
