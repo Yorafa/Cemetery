1e4d5b486b03365cfe48cd9c6497c6733dcd6138
Yorafa <thomasyorafa@gmail.com>
2024-04-11 16:43:16 -0400

    update ports

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 import nextConfig from './next.config.mjs'
 export const BasePath = nextConfig.basePath;
-export const BackendPath = "https://cemetery-6fef965af345.herokuapp.com";
+export const BackendPath = "yorafa.com:8000";
