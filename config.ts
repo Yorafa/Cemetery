bec1b1021b690d4088655f1713982358d0405e01
Yorafa <thomasyorafa@gmail.com>
2024-04-11 20:10:20 -0400

    fix https

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // import nextConfig from './next.config.mjs'
 export const BasePath = '../../';
-export const BackendPath = "http://yorafa.com:8000";
+export const BackendPath = "https://yorafa.com:8000";
