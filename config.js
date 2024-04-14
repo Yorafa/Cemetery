58a76a7af5febfb43d12670382214bc1514b84e8
Yorafa <thomasyorafa@gmail.com>
2024-04-14 18:03:38 -0400

    modify docker config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const PORT = 3000;
-// here use local mongodb with port 27017
-export const MONGO_URI = "mongodb://localhost:27017/cemetery";
+// here use local docker mongodb with port 27017
+export const MONGO_URI = "mongodb://mongo:27017/cemetery";
 
 export const CorsOptions = {
     origin: "https://cemetery.yorafa.com/",
